924f5766a5dcdc398f44763ea236826bf561b3db
Iryna <irynka1071994@gmail.com>
2020-03-29 23:40:57 +0300

    Updates

---
+++ precache-manifest.677b5429401a8a78aba1d9fda90c694f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6016c25aa103b97ae78d59d75ab38f3",
+    "url": "/ReactHW1/index.html"
+  },
+  {
+    "revision": "3cf763929d4467c6f83d",
+    "url": "/ReactHW1/static/css/main.7530eaa1.chunk.css"
+  },
+  {
+    "revision": "400e4adcc87ffda95ffc",
+    "url": "/ReactHW1/static/js/2.9fcdfcab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactHW1/static/js/2.9fcdfcab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf763929d4467c6f83d",
+    "url": "/ReactHW1/static/js/main.01cb26f3.chunk.js"
+  },
+  {
+    "revision": "8961983c42612de3c801",
+    "url": "/ReactHW1/static/js/runtime-main.a5dfa11b.js"
+  }
+]);
